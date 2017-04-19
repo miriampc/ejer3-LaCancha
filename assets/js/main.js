add7c169770c61ec51aab37b05776665c69b70a2
Miriam Peralta <miiriampc@gmail.com>
2017-04-19 12:06:28 -0500

    creando elementos con js

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+var objetosHijo=[{clase1:"arco", clase2:"left",claseNieto:"bolita"},
+                 {clase1:"arco",clase2:"right", claseNieto:"bolita"},
+                 {clase1:"circulo", clase2:"a", claseNieto:"bolita"}];
+
+window.addEventListener('load',function(){
+  console.log("fsdf");
+  var docFragment=document.createDocumentFragment();
+  var contenedor=document.createElement('section');
+  contenedor.setAttribute("id",'contenedor');
+
+  var cancha=document.createElement('div');
+  var borde=document.createElement('div');
+
+  cancha.classList.add("cancha");
+  borde.classList.add("borde");
+  cancha.appendChild(borde);
+
+  objetosHijo.forEach(function(e){
+    var hijo=document.createElement('div');
+    var nieto=document.createElement('div');
+    hijo.setAttribute("class",e.clase1);
+    hijo.classList.add(e.clase2);
+    nieto.setAttribute("class",e.claseNieto);
+
+    hijo.appendChild(nieto);
+    borde.appendChild(hijo);
+  });
+  contenedor.appendChild(cancha);
+  document.body.appendChild(contenedor);
+});
